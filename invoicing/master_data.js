9834a5303e19e1e015784888a1cd067aaea44e98
egavriel <thegavriel.co@gmail.com>
2026-04-01 16:00:42 +0700

    Update master_data.js

--- invoicing/master_data.js
+++ invoicing/master_data.js
@@ -12,6 +12,7 @@ const MASTER_DATA = {
     "Strawberry overnight oats::hanniel": { name: "Strawberry overnight oats", price: 42000, template: "hanniel" },
     "Double choco overnight oats::hanniel": { name: "Double choco overnight oats", price: 42000, template: "hanniel" },
     "Tiramisu overnight oats::hanniel": { name: "Tiramisu overnight oats", price: 42000, template: "hanniel" },
+"Banana Loaf::hanniel": { name: "Banana Loaf", price: 38000, template: "hanniel" },
     "Custom tin cookie hamper::hanniel-dp": { name: "Custom tin cookie hamper", price: 65000, template: "hanniel-dp" },
     "Custom tin cookie hamper::hanniel-fi": { name: "Custom tin cookie hamper", price: 65000, template: "hanniel-fi" },
     "Eid Royale Tin::eid": { name: "Eid Royale Tin", price: 188000, template: "eid" },
